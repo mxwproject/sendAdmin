ec546fc495afbf1e7722ea510b05531b3a9370f5
dlyt <miaosonglin@daoxila.com>
2018-06-20 16:31:24 +0800

    admin

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import iView from 'iview';
-// import 'iview/dist/styles/iview.css';
+import 'iview/dist/styles/iview.css';
 
 import axios from 'axios'
 
